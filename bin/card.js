dc84b413b893f8dfc21892854e7f10d67f579bfb
Alan Greene <github.com@alangreene.net>
2021-10-01 14:11:17 +0100

    Update dependencies and convert to ESM
    Minimum supported node version is 12.20 as it now requires ES module support.
    
    `__dirname` is no longer available so use `import.meta.url` instead.

--- bin/card.js
+++ bin/card.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const output = fs.readFileSync(path.join(__dirname, 'output'), 'utf8');
 console.log(output);
